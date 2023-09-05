219859e035c114fd3f831a7442670d24d8c16f80
william <jumawilliam2011@gmail.com>
2023-09-05 23:41:03 +0300

    form validation four ways

--- app/Livewire/Registeruser.php
+++ app/Livewire/Registeruser.php
@@ -1,27 +1,62 @@
 <?php
 
 namespace App\Livewire;
-use Livewire\Attributes\Rule;
+//use Livewire\Attributes\Rule;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class Registeruser extends Component
 {
-    #[Rule('required')]
+    //#[Rule('required', message: 'your name is required')]
     public $name;
 
-    #[Rule('required|unique:users|email')]
+    //#[Rule('required|unique:users|email')]
     public $email;
 
-    #[Rule('required|min:9')]
+    //#[Rule('required|min:9')]
     public $password;
 
     public function render()
     {
         return view('livewire.registeruser');
     }
-
+    
+    /*
+    public function rules(){
+        return[
+            'name'=>'required',
+            'email'=>'required|unique:users|email',
+            'password'=>'required|min:7'
+        ];
+    }
+    */
     public function save(){
-        $this->validate();
+       // $this->validate();
+        /*
+        $validated=$this->validate([
+            'name'=>'required',
+            'email'=>'required|unique:users|email',
+            'password'=>'required|min:7',
+        ]);
+        */
+        $validated=Validator::make(
+            //data that we are validating
+            [
+                'name'=>$this->name,
+                'email'=>$this->email,
+                'password'=>$this->password,
+
+            ],
+
+            [
+                'name'=>'required',
+                'email'=>'required|unique:users|email',
+                'password'=>'required|min:7',
+            ],
+
+            ['required'=>'The :attribute field is required']
+        )->validate();
         dd(123);
     }
+
 }
